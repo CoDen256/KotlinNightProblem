0a902c2cd03312c0062f023c8dfe9c04c57ff169
coden256 <den.blackshov@gmail.com>
2019-03-28 18:06:09 +0200

    created

---
+++ src/main.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>){
+    print(f(1))
+
+    println(convert(one))
+    for (num in nums){
+        println(replace(num))
+        println(count(num))
+    }
+}
+
+fun f(input: Int): String{
+
+    return ""
+}
+
+fun convert(num: String): String{
+    return num.replace(".", "Kotlin")
+}
+
+fun replace(n: String): String = n.replace("\n", "\\n")
+
+fun count(num: String): Int{
+    var n = 0
+    for (char in num){
+        if (char != ' ' && char != '\n'){
+            n ++
+        }
+    }
+    return n
+}
