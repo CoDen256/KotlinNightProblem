658d7772dc40e6cd4fb7240ecffcf46187d68cf7
coden256 <den.blackshov@gmail.com>
2019-03-29 12:06:06 +0200

    improved to 344

--- src/main.kt
+++ src/main.kt
@@ -1,113 +1,18 @@
 fun f(input: Int) : String{
-    val n = """3
-3
-.      .
-.      .
-.      .
-.      .
-.      .
-.      .
-3
-3
-            .
-      2
-    .  .
-  .    .
-.      .
-            .
-            .
-            .
-            .
-            .
-3
-3
-            .
-            .
-3
-3
-.
-.
-3
-3
-3
-3
-            .
-            .
-3
-3
-            .
-            .
-3
-3
-.      .
-.      .
-.      .
-.      .
-3
-3
-            .
-            .
-            .
-            .
-3
-3
-.
-.
-3
-3
-            .
-            .
-3
-3
-3
-3
-.
-.
-3
-3
-.      .
-.      .
-3
-3
-3
-3
-            .
-           .
-          .
-         .
-        .
-       .
-      .
-     .
-3
-3
-.      .
-.      .
-3
-3
-.      .
-.      .
-3
-3
-3
-3
-.      .
-.      .
-3
-3
-            .
-            .
-3
-3"""
+    val n = "7      2      2      2      2      2      7            1      2    1  1  1    2      1            1            1            1            1            7            1            17            1            66            1            7            1            17      2      2      2      16            1            1            1            17            1            6            1            77            1            61      2      76            1           1           1           1           1           1           1           1       7      2      17      2      77      2      16            1            7"
+        .apply {
 
-    fun String.r(n: Int) = replace("$n", ".".repeat(n))
+        }
+    fun String.r(n: Int) = replace("$n", "Kotlin".repeat(n))
     val a = arrayListOf<String>()
-    for (i in 0 until 10){
+    for (i in 0..9) {
         var s = ""
-        for (e in "$input"){
-            s += n.split("\n")["$e".toInt()*10 + i].r(3).r(2).replace(".", "Kotlin").padEnd(20)
+        for (e in "$input") {
+            val x = 18*("$e".toInt()*10 + i)
+            s += n.r(7).r(6).r(2).r(1).substring(x, x + 18).padEnd(20)
         }
         a.add(s.dropLast(1))
+
     }
     return a.joinToString("\n")
 }
